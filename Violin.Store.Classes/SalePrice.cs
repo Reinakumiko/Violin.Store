8652969ac4e038200119f15ce4095b127476c6cd
Reinakumiko <1164114736@qq.com>
2017-02-18 18:18:04 +0800

    Revert "update profile & pagefont & header animate"
    This reverts commit 9e3d13a16f48d5f4e71929ce083e2b14df056444.

--- Violin.Store.Classes/SalePrice.cs
+++ Violin.Store.Classes/SalePrice.cs
@@ -1,4 +1,10 @@
-﻿namespace Violin.Store.Classes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Violin.Store.Classes
 {
 
 	/// <summary>
--- Violin.Store.Database/DatabaseContext.cs
+++ Violin.Store.Database/DatabaseContext.cs
@@ -10,34 +10,11 @@ namespace Violin.Store.Database
 {
 	public class DatabaseContext : DbContext
 	{
-		/// <summary>
-		/// 商品表
-		/// </summary>
 		public DbSet<Goods> Goods { get; set; }
-
-		/// <summary>
-		/// 专辑表
-		/// </summary>
 		public DbSet<Discography> Discography { get; set; }
-
-		/// <summary>
-		/// 新闻表
-		/// </summary>
 		public DbSet<News> News { get; set; }
-
-		/// <summary>
-		/// 个人简介表
-		/// </summary>
 		public DbSet<Profile> Profile { get; set; }
-
-		/// <summary>
-		/// 日程表
-		/// </summary>
 		public DbSet<Schedule> Schedule { get; set; }
-
-		/// <summary>
-		/// 账户表
-		/// </summary>
 		public DbSet<UserAccount> Account { get; set; }
 
 
