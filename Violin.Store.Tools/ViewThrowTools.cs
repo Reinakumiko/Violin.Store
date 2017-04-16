55f328a39349a59937224a4cf195ba3e6902d8de
Reinakumiko <1164114736@qq.com>
2017-04-16 13:58:12 +0800

    update throwmessage comment

--- Violin.Store.Tools/ViewThrowTools.cs
+++ Violin.Store.Tools/ViewThrowTools.cs
@@ -11,6 +11,12 @@ namespace Violin.Store.Tools
 {
     public static class ViewThrowTools
     {
+        /// <summary>
+        /// 在控制器中抛出错误消息。
+        /// </summary>
+        /// <param name="controller">于 <see cref="Controller"/> 的扩展方法</param>
+        /// <param name="viewThrow">需要抛出的错误信息</param>
+        /// <returns></returns>
         public static ActionResult Throw(this Controller controller, ViewThrow viewThrow)
         {
             var jsonResult = JsonConvert.SerializeObject(new { errcode = (int)viewThrow.StatusCode, errmsg = viewThrow.Message });
@@ -19,6 +25,12 @@ namespace Violin.Store.Tools
             return new HttpStatusCodeResult(viewThrow.StatusCode, viewThrow.Message);
         }
 
+        /// <summary>
+        /// 在控制器中返回请求消息
+        /// </summary>
+        /// <param name="controller">于 <see cref="Controller"/> 的扩展方法</param>
+        /// <param name="viewThrow">需要返回到请求的数据内容</param>
+        /// <returns></returns>
         public static ActionResult RequestResult(this Controller controller, ViewThrow viewThrow)
         {
             var jsonResult = JsonConvert.SerializeObject(new { code = (int)viewThrow.StatusCode, result = viewThrow.Result, msg = viewThrow.Message });
